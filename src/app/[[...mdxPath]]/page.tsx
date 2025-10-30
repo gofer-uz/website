8df9ba79c79b74f78212faffea7d249d752cd43a
abdurahmon27 <bekzotovich12@gmail.com>
2025-10-30 23:16:02 +0500

    chore: init README.md

---
+++ src/app/[[...mdxPath]]/page.tsx
@@ -0,0 +1,27 @@
+import { importPage } from "nextra/pages";
+import { useMDXComponents as getMDXComponents } from "../../../mdx-components";
+
+
+
+export async function generateMetadata(props: any) {
+  const params = await props.params;
+  const { metadata } = await importPage(params.mdxPath);
+  return metadata;
+}
+
+const Wrapper = getMDXComponents().wrapper || (({ children }: any) => <>{children}</>);
+
+export default async function Page(props: any) {
+  const params = await props.params;
+  const {
+    default: MDXContent,
+    toc,
+    metadata,
+    sourceCode,
+  } = await importPage(params.mdxPath);
+  return (
+    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
+      <MDXContent {...props} params={params} />
+    </Wrapper>
+  );
+}
